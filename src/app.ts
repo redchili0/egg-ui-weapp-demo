514428a3100259a5454cd3bc0a5a6bba3c22b20c
ben <544652471@qq.com>
2020-07-29 16:40:53 +0800

    feat: add some feature.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import React from "react";
 // import '@redchili/egg-ui/dist/style/main.css'
-import './app.css';
+import "./app.css";
 
-const App: React.FC = props => props.children as React.ReactElement;
+const App: React.FC = (props) => props.children as React.ReactElement;
 
 export default App;
